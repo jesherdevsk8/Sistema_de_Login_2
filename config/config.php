044986464c7944ac0560016fa8f012470ae3b737
Jesher Minelli <jesherdevsk8@gmail.com>
2022-05-11 20:21:25 -0300

    Update dados banco

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 
-define('MYSQL_HOST','localhost:3307'); // local do banco de dados usb webserver
-// define('MYSQL_HOST','localhost'); //local do banco de dados
-define('MYSQL_USER','root'); // nome de usuario
-define('MYSQL_PASSWORD','usbw'); // senha de usuario usb webserver
-// define('MYSQL_PASSWORD',''); // senha de usuario
-define('MYSQL_DB_NAME','3ds_pw3'); // nome do banco de dados
+// define('MYSQL_HOST','localhost:3307'); // local do banco de dados usb webserver
+define('MYSQL_HOST','localhost'); //local do banco de dados
+define('MYSQL_USER','jesher'); // nome de usuario
+// define('MYSQL_PASSWORD','usbw'); // senha de usuario usb webserver
+define('MYSQL_PASSWORD','123'); // senha de usuario
+define('MYSQL_DB_NAME','logindb'); // nome do banco de dados
 
 try{
     $PDO = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD);
