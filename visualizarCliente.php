1e47d45996b77841e3fc6cdd4873106095bcf927
Jesher Minelli <jesherdevsk8@gmail.com>
2022-05-12 22:52:28 -0300

    nova feature deletar clientes

--- visualizarCliente.php
+++ visualizarCliente.php
@@ -35,18 +35,19 @@ $administrador  = $_SESSION['administrador'];
             <div class="col-md-offset-2 col-md-8">
                 <h2>Visualizar Clientes</h2>
                 <br>
-                <table class="table table-condensed table-hover table-responsive">
+                <table id="tbl_visualizar" class="table table-condensed table-hover table-responsive">
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>nome</th>
-                            <th>logradouro</th>
-                            <th>num</th>
-                            <th>cep</th>
-                            <th>cidade</th>
-                            <th>bairro</th>
-                            <th>telefone</th>
-                            <th>estado</th>
+                            <th>id_cliente</th>
+                            <th>Nome</th>
+                            <th>Logradouro</th>
+                            <th>Num</th>
+                            <th>CEP</th>
+                            <th>Cidade</th>
+                            <th>Bairro</th>
+                            <th>Telefone</th>
+                            <th>Estado</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -70,7 +71,7 @@ $administrador  = $_SESSION['administrador'];
 
 
                         ?>
-                                <tr>
+                                <tr id="<?= $id_cliente ?>">
                                     <td><?= $id_cliente ?></td>
                                     <td><?= $nome ?></td>
                                     <td><?= $logradouro ?></td>
@@ -81,8 +82,9 @@ $administrador  = $_SESSION['administrador'];
                                     <td><?= $telefone ?></td>
                                     <td><?= $estado ?></td>
                                     <td>
-                                        <a class="btn btn-info" data-toggle="tooltip" data-placement="top" title="Editar" href="editarCliente.php?id=<?= $id_cliente ?>"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
-                                        <a class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Deletar" href="include/deletarCliente.php?id=<?= $id_cliente ?>"><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span></a>
+                                        <center>
+                                            <a class="btn btn-info" data-toggle="tooltip" data-placement="top" title="Editar" href="editarCliente.php?id=<?= $id_cliente ?>"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
+                                        </center>
                                     </td>
                                 </tr>
                         <?php
@@ -90,6 +92,9 @@ $administrador  = $_SESSION['administrador'];
                         ?>
                     </tbody>
                 </table>
+                <a data-toggle="modal" data-target="#clientes_deletar" class="btn btn-danger" id="deletar_clientes">
+                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Deletar selecionados
+                </a>
             </div>
         </div>
         <!-- /row do formulario de login -->
@@ -100,6 +105,48 @@ $administrador  = $_SESSION['administrador'];
 
     <!-- Scripts JS -->
     <?php include('include/scripts.php'); ?>
-    <script src="js/deletarCliente.js"></script>
+    <script>
+        $(document).ready(function() {
+
+            // carrega a datable
+            $('#tbl_visualizar').DataTable({
+                responsive: true,
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+                }
+            });
+
+            // deletar
+
+            // seleciona a linha da tabela ao clicar e deixa com a classe danger
+            $('#tbl_visualizar tbody').on('click', 'tr', function () {
+                $(this).toggleClass('danger');
+            });
+
+            // ao clicar no botao de deletar, o codigo guarda os ids que estão com a classe danger e enviar os ids para o modal
+            $('#deletar_clientes').click(function () {
+
+                var values = $('.danger').map(function () {
+                    return this.id;
+                }).get();
+
+                $(this).attr("href", 'include/modal/clientesDeletar.php?ids=' + values);
+
+            });
+        
+            // limpa o cache do script
+            $("#clientes_deletar").on('hidden.bs.modal', function () {
+                $(this).data('bs.modal', null);
+            });
+
+        });
+    </script>
   </body>
 </html>
+<div id="clientes_deletar" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+        </div>
+    </div>
+</div>
